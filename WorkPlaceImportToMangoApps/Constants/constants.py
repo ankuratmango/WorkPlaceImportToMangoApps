7cb9867bef71b0262251a703544a94cc8a29b2a2
Ankur Tripathi <95279670+ankuratmango@users.noreply.github.com>
2024-11-25 12:25:01 +0530

    Added Constant file

---
+++ WorkPlaceImportToMangoApps/Constants/constants.py
@@ -0,0 +1,17 @@
+class Constants():
+
+    def __init__(self):
+        pass
+    
+    GRAPH_URL_PREFIX = 'https://graph.workplace.com/'
+    FIELDS_CONJ = '?fields='
+    CONVERSATIONS_SUFFIX = '/conversations'
+    MESSAGES_FIELDS = 'messages{id, created_time,message,to,from,attachments}'
+    JSON_KEY_DATA = 'data'
+    JSON_KEY_PAGING = 'paging'
+    JSON_KEY_NEXT = 'next'
+    JSON_KEY_EMAIL = 'email'
+
+
+
+
--- WorkPlaceImportToMangoApps/Main.py
+++ WorkPlaceImportToMangoApps/Main.py
@@ -1,1 +1,5 @@
+import sys
+from Constants.constants import Constants
 
+my_constant = Constants()
+print(my_constant.GRAPH_URL_PREFIX)
